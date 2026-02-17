22673a17f51d76f13c44d08e097803c3e28d236d
jerrickm <jerrickm@email.sc.edu>
2026-02-17 08:26:31 -0500

    examples update

--- examples/intro.js
+++ examples/intro.js
@@ -1,4 +1,4 @@
-//button click example
+///button click example
 document.getElementById("btn-show-message").onclick = ()=>{
     document.getElementById("p-message").innerHTML = "Hi Portia";
 };
@@ -76,11 +76,22 @@ setInterval(()=>{
     document.getElementById("p-date").innerHTML = `${hours}:${minutes}:${seconds}, ${month}/${day}/${year}`;
 }, 500);
 
-/* donation */
-document.getElementById ("btn-display-donation").onclick = () => {
-    const errorP = document.getElementById ("txt-donation").value;
+/* Donations */
+document.getElementById("btn-display-donation").onclick = () => {
+    const errorP = document.getElementById("p-donation-error");
+    errorP.innerHTML = "";  //if you click the button twice
+
+    const donationText = document.getElementById("txt-donation").value;
 
     if(isNaN(donationText) || donationText < 0){
-        errorP.innerHTML = (" Invalied amount")
+        errorP.innerHTML = "* Invalid amount";
+        return;
     }
+
+    donation = parseInt(donationText);
+    const percentGoal = donation/5000 * 100;
+
+    document.getElementById("p-donation").innerHTML = `You've reached ${percentGoal}% of your goal.`
+
+    document.querySelector(":root").style.setProperty("--donation",percentGoal + "%");
 }
